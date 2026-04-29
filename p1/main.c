2650ff097085987ee5fa846f5a371ea87b16587a
2025050125 <codnjs486012@kau.kr>
2026-04-29 15:36:13 +0900

    Update main.c

---
+++ p1/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+int main(){
+    int N;
+    int found=0;
+    scanf("%d",&N);
+    for(int a=1;a*900<N;a++){
+        for (int b=2;a*900+b*750<N;b+=2){
+        for(int c=1;a*900+b*750+c*200<=N;c++){
+  
+    int total=(a*900)+(b*750)+(c*200);
+    if (total==N){
+        if(c<a||c<b)  {
+    
+        printf("%d%d%d",a,b,c);
+    found=1;}
+    }
+        }
+    }
+}
+    if(!found){
+        printf("none\n")
+    }
+    return 0;
+    
+}
